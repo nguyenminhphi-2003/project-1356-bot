e16288a378f0d91a47311da53490be4a39b18562
Nguyễn Minh Phi <nmphi2710@gmail.com>
2026-01-04 13:51:04 +0700

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from telegram import Update
+
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from config.config import BOT_TOKEN
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
